acf5cb0afe56e488a3654f2bde6093410763d5bf
Hanjoo Kim <hachookimhanjoo@gmail.com>
2023-10-24 16:24:50 +0800

    Update tests

--- src/test/java/seedu/address/model/person/BloodTypeTest.java
+++ src/test/java/seedu/address/model/person/BloodTypeTest.java
@@ -1,8 +1,11 @@
 package seedu.address.model.person;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import seedu.address.model.shared.Name;
 
 public class BloodTypeTest {
 
@@ -21,5 +24,14 @@ public class BloodTypeTest {
     public void isValidBloodType() {
         // null tag name
         assertThrows(NullPointerException.class, () -> BloodType.isValidBloodType(null));
+    }
+
+    @Test
+    public void equals() {
+        BloodType bt = new BloodType("AB+");
+        assertTrue(bt.equals(bt));
+        assertTrue(bt.equals(new BloodType("AB+")));
+        assertFalse(bt.equals(new BloodType("AB-")));
+        assertFalse(bt.equals(new Name("Alice Pauline")));
     }
 }
