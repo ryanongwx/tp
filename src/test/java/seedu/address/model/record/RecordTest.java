91ea3bb6440d39c073d5d5cdb8edbd06686b2617
Hanjoo Kim <hachookimhanjoo@gmail.com>
2023-10-27 01:55:36 +0800

    Edit RecordTest

--- src/test/java/seedu/address/model/record/RecordTest.java
+++ src/test/java/seedu/address/model/record/RecordTest.java
@@ -2,7 +2,6 @@ package seedu.address.model.record;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static seedu.address.testutil.Assert.assertThrows;
-import static seedu.address.testutil.TypicalRecords.FEVER;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,25 +12,29 @@ import org.junit.jupiter.api.Test;
 import seedu.address.model.shared.DateTime;
 
 public class RecordTest {
+
+    private List<Condition> validConditions = new ArrayList<>(Arrays.asList(new Condition("Fever"),
+            new Condition("Cold")));
+    private List<Medication> validMedications = new ArrayList<>(Arrays.asList(new Medication("Tylenol"),
+            new Medication("Ibuprofen")));
+    private Record record = new Record(new DateTime("09-10-2023 1800"), validConditions, validMedications);
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new Record(null, null, null));
         assertThrows(NullPointerException.class, () -> new Record(new DateTime("09-10-2023 1800"),
                 null, null));
-        List<Condition> validConditions = new ArrayList<>(Arrays.asList(new Condition("Fever"), new Condition("Cold")));
-        List<Medication> validMedications = new ArrayList<>(Arrays.asList(new Medication("Tylenol"),
-                new Medication("Ibuprofen")));
+
         assertThrows(NullPointerException.class, () -> new Record(null, validConditions, validMedications));
     }
 
     @Test
     public void toString_success() {
         String expected = Record.class.getCanonicalName()
-                + "{dateTime=" + FEVER.getDateTime()
-                + ", conditions=" + FEVER.getConditions()
-                + ", medications=" + FEVER.getMedications()
+                + "{dateTime=" + record.getDateTime()
+                + ", conditions=" + record.getConditions()
+                + ", medications=" + record.getMedications()
                 + "}";
 
-        assertEquals(expected, FEVER.toString());
+        assertEquals(expected, record.toString());
     }
 }
