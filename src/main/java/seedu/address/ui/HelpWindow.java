efb37bbbfe6294bc7ded36d31981f2609a844469
Clin-lyx <110957868+Clin-lyx@users.noreply.github.com>
2023-10-29 16:19:40 +0800

    Update help page

--- src/main/java/seedu/address/ui/HelpWindow.java
+++ src/main/java/seedu/address/ui/HelpWindow.java
@@ -9,23 +9,65 @@ import javafx.scene.input.Clipboard;
 import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.commands.AddCommand;
+import seedu.address.logic.commands.AddRecordCommand;
+import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.EditRecordCommand;
+import seedu.address.logic.commands.ExitCommand;
+import seedu.address.logic.commands.FindCommand;
+import seedu.address.logic.commands.ListCommand;
 
 /**
  * Controller for a help page
  */
 public class HelpWindow extends UiPart<Stage> {
 
-    public static final String USERGUIDE_URL = "https://se-education.org/addressbook-level3/UserGuide.html";
-    public static final String HELP_MESSAGE = "Refer to the user guide: " + USERGUIDE_URL;
+    public static final String USERGUIDE_URL =
+            "https://ay2324s1-cs2103t-t12-4.github.io/tp/UserGuide.html";
+    public static final String HELP_MESSAGE = "A more detailed User "
+            + "Guide can be accessed" + "from this url: ";
 
+    public static final String COMMAND_SUMMARY = "COMMAND SUMMARY";
+    private static final String PIN_AND_UNPIN = "pin/unpin INDEX"
+            + "\n" + "pin / unpin a patient.";
+    private static final String VIEW_AND_DELETE = "view/delete INDEX"
+            + "\n" + "view / delete a patient from MedBook.";
     private static final Logger logger = LogsCenter.getLogger(HelpWindow.class);
     private static final String FXML = "HelpWindow.fxml";
 
     @FXML
     private Button copyButton;
-
+    @FXML
+    private Label commandSummary;
     @FXML
     private Label helpMessage;
+
+    @FXML
+    private Label addPatient;
+
+    @FXML
+    private Label editPatient;
+
+    @FXML
+    private Label addRecord;
+
+    @FXML
+    private Label editRecord;
+
+    @FXML
+    private Label pinAndUnpin;
+
+    @FXML
+    private Label list;
+
+    @FXML
+    private Label find;
+
+    @FXML
+    private Label viewAndDelete;
+
+    @FXML
+    private Label exit;
 
     /**
      * Creates a new HelpWindow.
@@ -34,7 +76,17 @@ public class HelpWindow extends UiPart<Stage> {
      */
     public HelpWindow(Stage root) {
         super(FXML, root);
-        helpMessage.setText(HELP_MESSAGE);
+        commandSummary.setText(COMMAND_SUMMARY);
+        helpMessage.setText(HELP_MESSAGE + USERGUIDE_URL);
+        addPatient.setText(AddCommand.MESSAGE_USAGE);
+        editPatient.setText(EditCommand.MESSAGE_USAGE);
+        addRecord.setText(AddRecordCommand.MESSAGE_USAGE);
+        editRecord.setText(EditRecordCommand.MESSAGE_USAGE);
+        find.setText(FindCommand.MESSAGE_USAGE);
+        pinAndUnpin.setText(PIN_AND_UNPIN);
+        viewAndDelete.setText(VIEW_AND_DELETE);
+        list.setText(ListCommand.COMMAND_WORD + "\n" + ListCommand.MESSAGE_SUCCESS);
+        exit.setText(ExitCommand.COMMAND_WORD + "\n" + ExitCommand.MESSAGE_EXIT_ACKNOWLEDGEMENT);
     }
 
     /**
